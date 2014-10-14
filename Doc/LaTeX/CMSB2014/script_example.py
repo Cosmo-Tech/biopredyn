9613204488d500c5a4fc7c87e6bebb66d6999384
Bertrand Moreau <bertrand.moreau@thecosmocompany.com>
2014-10-14 11:33:13 +0200

    CMSB 2014 abstract edited
    - fixed several typos
    - added currently implemented features
    - simplified example script (formatting issues)

--- Doc/LaTeX/CMSB2014/script_example.py
+++ Doc/LaTeX/CMSB2014/script_example.py
@@ -1,9 +1,8 @@
-from biopredyn import resources, workflow
-rm = resources.ResourceManager() # Mandatory resource manager
-wf = workflow.WorkFlow('training_3_3.xml', rm)
+from biopredyn import workflow, resources as rm
+wf = workflow.WorkFlow('wf_1.xml', rm.ResourceManager())
 for m in wf.get_models():
-  m.apply_changes()
-  m.write_sbml(m.get_id() + "_changed.xml")
-  m.init_tree() # Model reset
+  m.apply_changes() # Apply changes to current model
+  m.write_sbml(m.get_id() + "_changed.xml") # Write it
+  m.init_tree() # Reset model
 wf.run_tasks() # Execute work flow
 wf.process_outputs()
