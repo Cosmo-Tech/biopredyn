38df52c0c7610473c821c2a1681cc40f2dba8513
Bertrand Moreau <bertrand.moreau@thecosmocompany.com>
2015-01-23 15:59:52 +0100

    Updated headers
    - incremented year
    - removed unused header fields

--- Prototype/python/biopredyn/statistics.py
+++ Prototype/python/biopredyn/statistics.py
@@ -2,11 +2,8 @@
 # coding=utf-8
 
 ## @package biopredyn
-## $Author$
-## $Date$
-## $Copyright: 2014, The CoSMo Company, All Rights Reserved $
-## $License: BSD 3-Clause $
-## $Revision$
+## Copyright: [2012-2015] The CoSMo Company, All Rights Reserved
+## License: BSD 3-Clause
 
 import result
 import numpy as np
